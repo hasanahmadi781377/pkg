4d21a32b102fd49c12912c7bb141a48276245dc2
hasanahmadi781377 <hasanahmadi781377@gmail.com>
2023-11-17 15:06:38 +0330

    add requeirment

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 setup(
     name='pkg',
-    version='0.1',
+    version='1.2',
     description='this is for the test demo',
     url='https://github.com/hasanahmadi781377/pkg',
     author='Hasan ahmadi',
     author_email='hasanahmadi781377@gmail.com',
     license='MIT',
     packages=['pkg'],
+    install_requires=['Django'],
     zip_safe=False)
